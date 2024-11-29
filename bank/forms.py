79395b08ea37a31aeb958f008632ada7cf886e24
LuPatrik <patrik.luoto@gmail.com>
2024-11-30 00:29:57 +0200

    raised the minimum password length to 8

--- bank/forms.py
+++ bank/forms.py
@@ -10,22 +10,22 @@ class RegistrationForm(forms.ModelForm):
 
     password = forms.CharField(
         widget=forms.PasswordInput,
-        help_text="For security reasons it is recommended for the password to be at least 8 characters long."
+        help_text="For security reasons it is recommended for the password to be at least 10 characters long."
     )
 
     def clean_password(self):
         password = self.cleaned_data.get('password')
         #passwords are not checked for their security
-        """
+        
         has_letter = any(char.isalpha() for char in password)
         has_number = any(char.isdigit() for char in password)
         if not has_letter:
             raise ValidationError("Password must contain at least one letter!")
         if not has_number:
             raise ValidationError("Password must contain at least one number!")
-        if len(password) < 6:
-            raise ValidationError("Password must be at least 6 characters long!")
-        """
+        if len(password) < 8:
+            raise ValidationError("Password must be at least 8 characters long!")
+        
         return password
 
 class LoginForm(forms.Form):
